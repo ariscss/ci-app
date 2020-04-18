cedbd5b23deeba1eca54d9a282fabc45effb17f8
Aris Candra Saputra <aris.candra.s@gmail.com>
2020-04-18 14:23:04 +0700

    menambahkan isi about

---
+++ application/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="card text-white bg-danger">
+  <img class="card-img-top" src="holder.js/100px180/" alt="">
+  <div class="card-body">
+    <h4 class="card-title">JUDUL</h4>
+    <p class="card-text">Haii ini aplikasi pertamaq</p>
+  </div>
+</div>
